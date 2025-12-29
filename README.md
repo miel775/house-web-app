1c017776f3b4c5c324379b02af7d246daf68c2b1
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-12-29 10:46:41 +0100

    build: header
    made the first step with header component
    
    #10

--- README.md
+++
@@ -1,2 +0,0 @@
-# house-web-app
-intern-assessment
---
+++ house/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <a>Houses</a>
+        <a>About</a>
+    </header>
+</template>
+
+<style scoped>
+header {
+    position: fixed;
+    width: 100vw;
+    height: 10vh;
+    background-color: white;
+}
+
+</style>
