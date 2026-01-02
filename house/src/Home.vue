bc991a1fcf7862aa31d7e7b6f8baac04d9d4a1c4
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-01-02 12:26:05 +0000

    style: style-sheet deleted
    Delete: the stylesheet of vue
    
    #1

--- house/src/Home.vue
+++ house/src/Home.vue
@@ -1,8 +1,20 @@
 <script setup>
-    import Header from './components/Header.vue';
 </script>
 
 <template>
-    <Header> </Header>
     <h1>Home</h1>
+    <p>Welcome on the house page</p>
+
+    <img class="background" src="./assets/background.png">
 </template>
+
+<style>
+    .background {
+        position: absolute;
+        z-index: -1;
+        height: 100vh;
+        width: 100vw;
+        left: 0;
+        top: 0;
+    }
+</style>
