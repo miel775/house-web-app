5c98a3f1a9144b657b8194a310a6514634a90383
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-01-08 14:45:21 +0100

    style: recommended houses
    recommended on the home page are loaded now
    
    #26

--- house/src/Home.vue
+++ house/src/Home.vue
@@ -40,13 +40,17 @@ onMounted(async () => {
   <img :src="houses[1].image" alt="House image">
 </div>
         <h1>Home</h1>
-        <p>Welcome on the house page</p>
+        <p>Welcome on the house page on this page you can navigate through different houses. Navigate to the house button to explore the houses.</p>
 
+        <h2> Recommended houses</h2>
         <div class="recommended-houses">
             <article>
-                <div v-for="house in houses" :key="house.id">
+                <div v-for="house in houses.slice(0, 4)" :key="house.id">
                     <a>
-                        <img :src="house.image" alt="House image" width="250px">
+                        <img :src="house.image" alt="House of {{ house.location.street }} {{ house.location.houseNumber }} ">
+                        <div class="street-name-hover"> 
+                            <p> {{ house.location.street }} {{ house.location.houseNumber }} </p>
+                        </div>
                     </a>
                 </div>
             </article>
@@ -74,9 +78,48 @@ onMounted(async () => {
         top: -100px;
         position: relative;
     }
+
+    @media (max-width: 720px) {
+      height: 40vh;
+
+      img {
+      width: 200vw;
+      }
+  }
+}
+
+.recommended-houses {
+  height: 30vh;
+  width: 100vw;
+  position: absolute;
+  overflow-y: scroll;
+  background-color: var(--primary-color-light);
+  padding: 2em;
 }
 
 .recommended-houses article {
     display: flex;
+    gap: 1em;
+    aspect-ratio: 1/1;
+
+    img {
+      overflow: hidden;
+      width: 256px;
+      height: 256px;
+    }
+
+    img ~ .street-name-hover {
+      display: absolute;
+      top: 0;
+      left: 0;
+      z-index: 2;
+        background-color: var(--primary-color);
+  border: var(--border-white)
+    }
+}
+
+.street-name-hover {
+  display: none;
+
 }
 </style>
