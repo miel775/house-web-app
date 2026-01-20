cff6e637103ca3872b532735deb6ea7e0704acab
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-01-20 17:09:41 +0100

    build: useRoute
    With use route it will recognize the param
    
    #18

--- house/src/pages/HouseDetail.vue
+++ house/src/pages/HouseDetail.vue
@@ -2,7 +2,12 @@
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 
-const houses = ref([])
+// useRoute to get the slug of a house
+const route = useRoute()
+const houseId = route.params.id
+
+// houses to houe
+const house = ref([])
 const error = ref(null)
 const loading = ref(false)
 
@@ -11,8 +16,7 @@ onMounted(async () => {
   
   try {
     const response = await fetch(
-      'https://api.intern.d-tt.nl/api/houses/:id'
-,
+      `https://api.intern.d-tt.nl/api/houses/${houseId}`,
       {
         headers: {
           'X-Api-Key': 'nvmE37kpr-xfUcAejPoM6_hC2Xbui5WO'
@@ -24,7 +28,7 @@ onMounted(async () => {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     
-    houses.value = await response.json();    
+    house.value = await response.json();    
   } catch (err) {
     error.value = err.message;
     console.error('Error fetching houses:', err);
@@ -42,6 +46,20 @@ onMounted(async () => {
 >
   <img src="/src/assets/icons/back.svg" width="48">
 </router-link>  
+  <div v-if="loading">Loading...</div>
+    <div v-else-if="house">
+      <img :src="house.image" width="400" />
+      <h1>{{ house.street }}</h1>
+
+      <p>€ {{ house.price }}</p>
+      <p>{{ house.zip }}</p>
+
+      <p>
+        {{ house.rooms.bedrooms }} beds ·
+        {{ house.rooms.bathrooms }} baths ·
+        {{ house.size }} m²
+      </p>
+    </div>
 
    <div class="recommended-houses">
 
