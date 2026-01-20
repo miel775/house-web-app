3ccbc7bc38661ccaded11c430198e210c09f21e8
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-01-20 17:30:51 +0100

    fix: (null)
    #18

--- house/src/pages/HouseDetail.vue
+++ house/src/pages/HouseDetail.vue
@@ -7,16 +7,22 @@ const route = useRoute()
 const houseId = route.params.id
 
 // houses to houe
-const house = ref([])
+const house = ref(null)
 const error = ref(null)
 const loading = ref(false)
 
+// value of house
+
+
 onMounted(async () => {
-  loading.value = true;
-  
   try {
+    loading.value = true
+
+    if (!route.params.id) {
+      throw new Error('House ID is missing')
+    }
     const response = await fetch(
-      `https://api.intern.d-tt.nl/api/houses/${houseId}`,
+      `https://api.intern.d-tt.nl/api/houses/${route.params.id}`,
       {
         headers: {
           'X-Api-Key': 'nvmE37kpr-xfUcAejPoM6_hC2Xbui5WO'
@@ -28,14 +34,14 @@ onMounted(async () => {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     
-    house.value = await response.json();    
+    house.value = await response.json()  
   } catch (err) {
     error.value = err.message;
     console.error('Error fetching houses:', err);
   } finally {
     loading.value = false; 
   }
-})  
+})
 </script>
 
 <template>
@@ -46,20 +52,10 @@ onMounted(async () => {
 >
   <img src="/src/assets/icons/back.svg" width="48">
 </router-link>  
-  <div v-if="loading">Loading...</div>
-    <div v-else-if="house">
-      <img :src="house.image" width="400" />
-      <h1>{{ house.street }}</h1>
 
-      <p>€ {{ house.price }}</p>
-      <p>{{ house.zip }}</p>
-
-      <p>
-        {{ house.rooms.bedrooms }} beds ·
-        {{ house.rooms.bathrooms }} baths ·
-        {{ house.size }} m²
-      </p>
-    </div>
+    <p v-else-if="house">
+      {{ house.zip }}
+    </p>
 
    <div class="recommended-houses">
 
