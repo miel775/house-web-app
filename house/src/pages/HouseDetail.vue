61128d9f877bc9d62d142d046b1b1aba44412f10
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-01-24 17:14:43 +0100

    gouogu[gou[0

--- house/src/pages/HouseDetail.vue
+++ house/src/pages/HouseDetail.vue
@@ -2,80 +2,42 @@
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 
-// useRoute to get the slug of a house
 const route = useRoute()
-const houseId = route.params.id
 
-// houses to houe
-const house = ref(null)
-const error = ref(null)
-const loading = ref(false)
-
-// value of house
-
-
-onMounted(async () => {
-  try {
-    loading.value = true
-
-    if (!route.params.id) {
-      throw new Error('House ID is missing')
-    }
-    const response = await fetch(
-      `https://api.intern.d-tt.nl/api/houses/${route.params.id}`,
-      {
-        headers: {
-          'X-Api-Key': 'nvmE37kpr-xfUcAejPoM6_hC2Xbui5WO'
-        }
-      }
-    )
-    
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    
-const data = await response.json()
-house.value = data[0]
-    
-  } catch (err) {
-    error.value = err.message;
-    console.error('Error fetching houses:', err);
-  } finally {
-    loading.value = false; 
+const houses = [
+  {
+    name: 'Theolasingstraat 16 Amsterdam',
+    image: 'https://verbouw-gigant.nl/wp-content/uploads/2018/02/Kosten-huis-laten-bouwen-1024x576.jpg',
+    zip: 'NO90ND',
+    price: 140000,
+    bedrooms: 3,
+    bathrooms: 2,
+    size: 60
+  },
+  {
+    name: 'Another House',
+    image: 'https://via.placeholder.com/250',
+    zip: 'AB12CD',
+    price: 200000,
+    bedrooms: 4,
+    bathrooms: 3,
+    size: 120
   }
-})
+]
 </script>
 
 <template>
   <main>
-   <router-link
-  class="house-listing-button"
-  :to="`/houses/`"
->
-  <img src="/src/assets/icons/back.svg" width="48" height="48">
-  <p> Back to overview</p>
-</router-link>  
 
-    <div class="title-house-listing-detail">
-
-      <!-- all the data needed -->
-      {{ house.location.street }} {{ house.location.houseNumber }}
-
-      {{ house.location.zip }} {{ house.location.city }}
-
-      {{ house.price }}
-
-      {{ house.size }}
-
-      Build at {{ house.constructionYear }}
-
-      {{ house.rooms.bathrooms }}
-      {{ house.rooms.bedrooms }}
-
-      {{ house.hasGarage }}
-
-      {{ house.description }}
+  <div class="house-container">
+    <div v-for="house in houses" :key="house.name" class="house-listing">
+      <img :src="house.image" :alt="house.name" class="house-listing-image" width="250px">
+      <h2>{{ house.name }}</h2>
+      <p>{{ house.zip }}</p>
+      <p>€{{ house.price }}</p>
+      <p>{{ house.bedrooms }} Bedrooms, {{ house.bathrooms }} Bathrooms, {{ house.size }} m²</p>
     </div>
+  </div>
 
    <div class="recommended-houses">
 
@@ -84,3 +46,79 @@ house.value = data[0]
 
   </main>
 </template>
+
+<style>
+.house-container {
+  container-type: inline-size;
+  container-name: house-listing;
+}
+
+.house-listing {
+    background-color: var(--neutral-color);
+    padding: 1em;
+    display: grid;
+    grid-template-columns: 1fr 70%;
+
+    .house-listing-image {
+        width: max(250px, 100%);
+        object-fit: cover;
+    }
+}
+
+.house-listing-details-2 {
+    display: flex;
+    gap: 2em;
+}
+
+.house-listing-details {
+    display: flex;
+    gap: 2em;
+}
+
+.house-listing h2 {
+  font-size: 1em;
+}
+
+@container house-listing (width < 720px) {
+  .house-listing h2 {
+    font-size: 2em;
+  }
+
+  .house-listing {
+    display: grid;
+    margin-right: 4%;
+    grid-template-columns: 1fr;
+  }
+}
+
+.house-listing-grid-one {
+  height: 100%;
+}
+
+.house-listing-details {
+  display: grid;
+  grid-template-rows: 40% 20% 10%;
+  margin: 1em;
+}
+
+.house-lising-details-1 div {
+  display: flex;
+  align-items: center;
+}
+
+.house-listing-details-2 div {
+  display: flex;
+  gap: 0.5em;
+}
+
+.house-listing-button {
+  width: 8em;
+  height: 2em;
+  background-color: var(--primary-color-lighter);
+}
+
+.house-listing-button-area {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
